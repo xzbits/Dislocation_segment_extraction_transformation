f298b6b735a65ed6774cb7fffabd6da4d6137c38
xzbits <phongtran2701@gmail.com>
2022-02-21 19:15:52 +0100

    Update main.py and .gitignore

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-# from src.ovito_extract import DumpDirectory
-from src.extract_data_allFrame import ExtractedData
+from src.ovito_extract import DumpDirectory
+from src.plot_multiple_temperature import MultipleTemperature
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    # path_dumpfile = r""
-    # path_save_file = r""
-    # temperature_list = [1200]
-    # dump_dir = DumpDirectory(path_dumpfile, path_save_file, temperature_desire=temperature_list)
-    # dump_dir.create_dir()
-    # dump_dir.generate_csv()
+    path_dumpfile = r""
+    path_save_file = r""
+    temperature_list = []
+    dump_dir = DumpDirectory(path_dumpfile, path_save_file, temperature_desire=temperature_list)
+    dump_dir.create_dir()
+    dump_dir.generate_csv()
 
-    csv_path = r"C:\Users\Hoa\Desktop\cur_work\1200_data\extract_file\extracted_data\t_1200"
-    data_wp = ExtractedData(convert_file_pth=csv_path,
-                            file_suffix="segment_points_con_frame_",
-                            group_by_axes=2,
-                            sort_by_axes=0,
-                            no_segment_per_group=2,
-                            length_lower_threshold=250,
-                            rms_length_list=[0.9999, 1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32, 1/64, 1/108],
-                            rerun=True)
+    # Average alloy
+    temperature_list = []
+    plot_path_temp_aa = r""
+    dir_suffix_aa = "t_"
+    csv_suffix_aa = "segment_points_con_frame_"
+    multiple_temperature_aa = MultipleTemperature(plot_path_temp_aa,
+                                                  dir_suffix_aa,
+                                                  csv_suffix_aa,
+                                                  length_lower_threshold=250,
+                                                  temp_list=temperature_list)
+    plot_data_aa = multiple_temperature_aa.multiple_temperature_wv_psd
+    rms = plot_data_aa["t_1200_rms"]
+    wv_psd = plot_data_aa["t_1200_wv_psd"]
 
-    rms = data_wp.rms
-    wv_psd = data_wp.af_wv_psd
-    print(rms.shape)
     rms_seg_1 = rms[:, 0]
     rms_seg_2 = rms[:, 2]
     rms_seg_3 = rms[:, 4]
@@ -34,8 +35,8 @@ if __name__ == "__main__":
     t_seg_4 = rms[:, 7]
 
     plt.loglog(t_seg_1, rms_seg_1, "b-")
-    plt.loglog(t_seg_2, rms_seg_2, "r-", label="dislocation segment 2, RMS = 110.568")
-    plt.loglog(t_seg_3, rms_seg_3, "g-", label="dislocation segment 3, RMS = 240.414")
-    plt.loglog(t_seg_4, rms_seg_4, "y-", label="dislocation segment 4, RMS = 277.3147")
+    plt.loglog(t_seg_2, rms_seg_2, "r-")
+    plt.loglog(t_seg_3, rms_seg_3, "g-")
+    plt.loglog(t_seg_4, rms_seg_4, "y-")
 
     plt.show()
